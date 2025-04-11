7df87d81307a891f93a8dd5fecbba43651831ea7
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2025-04-10 20:18:44 -0500

    continuing to modify navbar and added write/sign-in links

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
+
   return (
     <nav className="navbar">
       <Link to="/">
-        <div className="w-full text-4xl">The Bent Bindings</div>
+        <div className="w-full text-4xl">the bent bindings</div>
       </Link>
 
-      <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto">
+      <div
+        className={
+          "absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " +
+          (searchBoxVisibility ? "show" : "hide")
+        }
+      >
         <input
           type="text"
           placeholder="Search"
@@ -17,9 +24,21 @@ const Navbar = () => {
         <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
       </div>
       <div className="flex items-center gap-3 md:gap-6 ml-auto">
-        <button className="md:hidden bg-grey w-12 h-12 rounded full">
-          <i className="fi fi-rr-search text-2xl"></i>
+        <button
+          className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
+          onClick={() => setSearchBoxVisibility((currentVal) => !currentVal)}
+        >
+          <i className="fi fi-rr-search text-xl"></i>
         </button>
+
+        <Link to="/editor" className="hidden md:flex gap-2 link">
+          <i class="fi fi-ts-script"></i>
+          <p>Write</p>
+        </Link>
+
+        <Link className="btn-dark">
+        Sign in
+        </Link>
       </div>
     </nav>
   );
